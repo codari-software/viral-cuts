63f2c36e2d17e5342b3bd4c7b11b7f408235ea48
codari-software <suporte@codariapp.com.br>
2025-12-14 12:39:44 -0300

    feat: Create API entry point and update Vercel rewrites to route requests to it.

---
+++ api/index.ts
@@ -0,0 +1,7 @@
+
+// Import from the server directory
+// Note: Relative path depends on where 'project/api' is relative to 'project/server'
+// 'project/api/index.ts' -> '../server/src/index'
+import app from '../server/src/index';
+
+export default app;
